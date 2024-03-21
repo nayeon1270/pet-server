eb831425667960f853d2a051db1a7d3c0cc43a9e
nayeon <ally0419naver.com>
2024-03-21 12:41:26 +0900

    데이터베이스 연결중

--- index.js
+++ index.js
@@ -1,31 +1,74 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
+const models=require('./models');
 var port="8080" //사용할 서버 포트
+
+
 app.use(express.json());
 app.use(cors());
-
 //서버생성(request: 요청 , responsive: 응답)
 app.get('/products', (req, res) =>{
+   models.Product.findAll({
+    order:[
+        ['createdAt', 'DESC'],
+        //ASC: 오름차순, DESC: 내림차순
+        
+    ],
+    attributes:["id", "name", "description", "price", "seller", "createdAt", "imageUrl"],
+   })
+   .then((result)=>{
+    console.log("PRODUCT :",result);
     res.send({
-        products:[
-            {id:1, name:'고양이 장난감', price:16100, seller:'에구에구', imageUrl:'img/products/toy01.jpg'},
-            {id:2, name:'네이처 서클 캣토이', price:16700, seller:'티티켓', imageUrl:'img/products/toy02.jpg'},
-            {id:3, name:'셀프오뚜기', price:18500, seller:'힐링타임', imageUrl:'img/products/toy03.jpg'},
-            {id:4, name:'고양이 스크래처', price:13600, seller:'우프우프', imageUrl:'img/products/toy04.jpg'},
-        ]
+        product: result,
     })
+   })
+   .catch((error)=>{
+    console.error(error);
+    res.send("에러발생")
+   })
 })
-app.post('/products', (req, res) =>{
+app.get('/products/:id', (req, res) => {
     const params=req.params;
-    const {id, eventId} = params;
-    res.send('id는 ${id}와 ${eventId}입니다')
+    const {id} = params;
+    models.Product.findOne({
+        where: {
+            id:id,
+
+        }
+    }).then((result)=>{
+        console.log("product:", result);
+        res.send({
+            product: result
+        })
+    }).catch(()=>{
+        console.error(error);
+        res.send('상품조회시 에러가 발생했습니다.')
+    })
 })
 app.post('/products', (req, res) =>{
-    const query=req.query;
-    res.send({
-        body:body
+    const body=req.body;
+    const {name, price, seller, description} =body;
+    if(!name || !price || !seller || !description){
+        res.send('모든 필드를 입력해주세요.')
+    }
+
+    models.Product.create({
+        name,
+        description,
+        price,
+        seller
+
     })
+    .then((result)=>{
+        console.log('상품생성결과:', result);
+        res.send({result,})
+    })
+    .catch((error)=>{
+        console.error(error)
+        res.send('상품 업로드에 문제가 발생했습니다.')
+    })
+    
 })
 app.post('/Login', (req, res) =>{
     res.send('로그인 해주세요')
@@ -33,4 +76,13 @@ app.post('/Login', (req, res) =>{
 
 app.listen(port, () =>{
     console.log('펫 서버가 돌아가고 있습니다. ')
+    models.sequelize.sync()
+    .then(()=>{
+        console.log('DB연결 성공')
+    })
+    .catch((err)=>{
+        console.log(err);
+        console.log('DB연결 에러')
+        process.exit();
+    })
 })
